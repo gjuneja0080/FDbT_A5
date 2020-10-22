ebe8cf3b80b0cb0ac8e7afa7aa9479c94ce9fdfe
Gopal Juneja <gopaljuneja@Gopals-MBP.home>
2020-10-22 23:03:47 +0100

    Added procfile and requirements

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder='/Users/gopaljuneja/Desktop/Assignment5/server/')
 
 @app.route('/')
-#def hello_world():
- #return 'COVID-19 made me eat more green'
+
 def ibsHW():
     return render_template("ibsHW.html")
