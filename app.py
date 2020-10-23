1e0974305bb2608f967e2449ac77a86888b2c56b
Gopal Juneja <gopal.juneja03@gmail.com>
2020-10-23 01:06:51 +0100

    Updated add.py again(2x)

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from flask import Flask, render_template
 import os
-app = Flask(__name__)
+app = Flask(__name__, template_folder="/Users/gopaljuneja/Desktop/Assignment5/server/")
 
 HOST = '127.0.0.1'
 PORT = int(os.environ.get('PORT', 5000))
 DEBUG_MODE = True
 
-
-
-
-@app.route('/Users/gopaljuneja/Desktop/Assignment5/server/')
+@app.route('/')
 def ibsHW():
     return render_template("ibsHW.html")
 
