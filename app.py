25219e60b3abc4f8ec47483343c895be793c8430
Gopal Juneja <gopal.juneja03@gmail.com>
2020-10-25 09:34:15 +0000

    Displaying Actor data

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ PORT = int(os.environ.get('PORT', 5000))
 DEBUG_MODE = True
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = Falsev
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://imperial:imperial-fdt-online-2019-colossal-shelf@imperial-2020.ckp3dl3vzxoh.eu-west-2.rds.amazonaws.com:5432/dvdrental"
 db = SQLAlchemy(app)
 
@@ -30,9 +30,18 @@ class Film(db.Model):
 
   copies = relationship('Inventory')
 
-
   def __repr__(self):
     return 'FILM: title is ' + self.title
+
+class Actor(db.model):
+    __tablename__ = 'actor'
+    actor_id = db.Column(db.String(255), primary_key=True, unique=True)
+    first_name = db.Column(db.String(255), index=True)
+    last_name =  db.Column(db.String(255), index=True)
+    last_update = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return 'actor_id: ' + self.actor_id, 'first_name: ' + self.first_name 
 
 @app.route('/')
 def ibsHW():
@@ -43,5 +52,9 @@ def films():
   films = Film.query.all()
   return render_template('films.html', films = films)
 
+@app.route('/actors')
+def actors():
+    actors = Actor.query.all()
+    return render_template('actors.html', actors = actors)
 if __name__ ==  '__main__':
     app.run(debug=DEBUG_MODE,host=HOST, port=PORT)
